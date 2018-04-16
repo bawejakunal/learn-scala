3d682bd59bfc802a8916f48dee39f82bbf9b7031
Kunal Baweja <bawejakunal15@gmail.com>
2018-04-15 23:05:02 -0700

    scala classes

---
+++ basics/Classes.scala
@@ -0,0 +1,27 @@
+/**
+ * Class with no parameters
+ */
+
+class Calculator {
+  val brand = "HP"
+  def add(x: Int, y: Int): Int = x + y
+}
+
+class BrandedCalculator(brand: String) {
+    // everything defined outside a method in a class
+    // definition is considered as constructor
+    val color: String = brand match {
+        case "TI" => "blue"
+        case "HP" => "black"
+        case _ => "white"
+    }
+
+    def multiply(x: Int, y: Int): Int = x * y
+}
+
+val calc = new Calculator
+println(calc.add(1, 2))
+
+val brandedCalc = new BrandedCalculator("TI")
+println(brandedCalc.color)
+println(brandedCalc.multiply(5, 6))
--- basics/CurriedFunction.scala
+++ basics/CurriedFunction.scala
@@ -1,7 +1,7 @@
 def multiply(m: Int)(n: Int):Int = m * n
 println(multiply(2)(3))
 
-val timesTwo = multiply(2)(_)
+val timesTwo = multiply(2)_
 println(timesTwo(2))
 
 def add(x: Int, y: Int, z: Int): Int = x + y + z
@@ -10,6 +10,3 @@ val add3 = curriedAdd(3)
 val add7 = add3(4)
 println(add7(10))
 
-
-val timesThree = multiply(3)
-println(timesThree(3))
