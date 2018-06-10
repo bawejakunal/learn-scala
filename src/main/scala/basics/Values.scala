0b22c4973a815074ce6049e7d677f6d1010cbddc
Kunal Baweja <bawejakunal15@gmail.com>
2018-06-10 01:54:40 -0700

    try pre-commit hook script

--- src/main/scala/basics/Values.scala
+++ src/main/scala/basics/Values.scala
@@ -1,7 +1,9 @@
 package basics
 
 object Values extends App {
-  // a value is a final declaration i.e binding can not change
+// a value is a final declaration i.e binding can not change
   val two = 1 + 1
+  val three = two + 1
   println(two)
+  println(s"${three}")
 }
